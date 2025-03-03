76eb3910020f33be644337981b4eaf59a5941ae6
Datodia <dato.diasamidze.02@gmail.com>
2025-03-04 00:43:44 +0400

    fix: remove unnecessary checkbox

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,11 +1,10 @@
 
 "use client";
 
-import { useForm, Controller } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -20,7 +19,6 @@ const schema = z.object({
     .regex(georgianPhoneNumberRegex, 'ნომრის ფორმატი არასწორია'),
   email: z.string().email("არასწორი იმეილის ფორმატი"),
   password: z.string().min(6, "პაროლი უნდა იყოს 6 სიმბოლოზე მეტი"),
-  rememberMe: z.boolean().optional().default(false)
 });
 
 type FormData = z.infer<typeof schema>;
@@ -33,9 +31,6 @@ export default function SignUp() {
     formState: { errors },
   } = useForm<FormData>({
     resolver: zodResolver(schema),
-    defaultValues: {
-      rememberMe: false
-    }
   });
 
   const onSubmit = (data: FormData) => {
@@ -61,27 +56,6 @@ export default function SignUp() {
         <label htmlFor="password">პაროლი</label>
         <input id="passwrod" className="border-2 border-border rounded-sm my-2 p-2"  {...register('password')} type="password" placeholder="შეიყვანეთ პაროლი" />
         {errors.password ? <p className="text-destructive italic text-sm">{errors.password.message}</p> : null}
-      </div>
-      <div className="flex items-center space-x-2">
-        <Controller
-          name="rememberMe"
-          control={control}
-          render={({ field }) => (
-            <>
-              <Checkbox
-                id="rememberMe"
-                checked={field.value}
-                onCheckedChange={field.onChange}
-              />
-              <label
-                htmlFor="rememberMe"
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-              >
-                დაიმახსოვრე პაროლი
-              </label>
-            </>
-          )}
-        />
       </div>
       <Button type="submit">რეგისტრაცია</Button>
       <Link className="text-center font-medium flex mx-auto" href={'/auth/sign-in'}>ავტორიზაცია</Link>
