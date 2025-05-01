7a2756cc5d57171d84e4b4d0cdbe49a55a4ab1e2
Datodia <dato.diasamidze.02@gmail.com>
2025-05-02 00:09:44 +0400

    fix: homepage refresh blog view cound bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
-import HomePage from "@/components/pages/home";
+import Blogs from "@/components/layout/blogs";
+import Courses from "@/components/layout/courses";
+import Students from "@/components/layout/students";
+import { getAllChallenges } from "@/app/challenges/services";
+import { getAllBlogs } from "@/app/blogs/services";
+import { getAllCourses } from "@/app/courses/services";
+import { getAllFeedbacks } from "@/app/feedbacks/services";
+import Challenges from "@/components/layout/challenges";
 
-export const dynamic = 'force-dynamic';
-
-export default function Home() {
+export default async function Home() {
+  const [challenges, blogs, courses, feedbacks] = await Promise.all([
+    getAllChallenges('/challenges', 'take=6'),
+    getAllBlogs('/blogs', 'take=3'),
+    getAllCourses('/courses'),
+    getAllFeedbacks('/feedbacks')
+  ])
+  
   return (
-      <div className="max-w-[1240px] mx-auto">
-        <HomePage />
-      </div>
+    <div className="max-w-[1240px] mx-auto">
+      <Challenges challenges={challenges} />
+      <Blogs blogs={blogs} />
+      <Courses courses={courses} />
+      <Students feedbacks={feedbacks} />
+    </div>
   );
 }
